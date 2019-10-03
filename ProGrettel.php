969dfb7eedb540a5bd3346ded7a9edf688c40f6e
Eric Sanchez <ericsh40@hotmail.com>
2019-10-03 08:50:48 -0500

    first commit

---
+++ ProGrettel.php
@@ -0,0 +1,15 @@
+<?php
+$Ponderacion1 = $_POST["Ponderacion1"];
+$Ponderacion2 = $_POST['Ponderacion2'];
+$Ponderacion3 = $_POST['Ponderacion3'];
+$Ponderacion4 = $_POST['Ponderacion4'];
+$Ponderacion5 = $_POST['Ponderacion5'];
+
+echo "<h2>Hola</h2>";
+echo "Ponderacion1 = ".$Ponderacion1. "<br/>";
+echo "Ponderacion2 = ".$Ponderacion2. "<br/>";
+echo "Ponderacion3 = ".$Ponderacion3. "<br/>";
+echo "Ponderacion4 = ".$Ponderacion4. "<br/>";
+echo "Ponderacion5 = ".$Ponderacion5. "<br/>";
+
+?>
